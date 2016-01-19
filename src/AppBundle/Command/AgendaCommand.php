b178b1aa58cb6f76de3f9850cbe20b618db95634
Eduardo Martinez Cobos <eemece2@gmail.com>
2016-01-19 19:34:03 +0100

    Base command

---
+++ src/AppBundle/Command/AgendaCommand.php
@@ -0,0 +1,23 @@
+<?php
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AgendaCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('agenda:import')
+            ->setDescription('Agenda import');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Agenda Import</info>');
+    }
+}
