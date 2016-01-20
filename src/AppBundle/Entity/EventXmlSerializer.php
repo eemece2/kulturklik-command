46b2f121f48de02a3401d326050f5c3ad0c5cc62
Eduardo Martinez Cobos <eemece2@gmail.com>
2016-01-20 21:36:29 +0100

    complete EventXmlSerializer

--- src/AppBundle/Entity/EventXmlSerializer.php
+++ src/AppBundle/Entity/EventXmlSerializer.php
@@ -16,20 +16,29 @@ class EventXmlSerializer {
         $documentdescription = $eventElement->getElementsByTagName('documentdescription')->item(0)->textContent;
         $eventenddate = $this->deserializeDate($eventElement->getElementsByTagName('eventenddate')->item(0)->textContent);
         $eventonline = $this->deserializeBoolean($eventElement->getElementsByTagName('eventonline')->item(0)->textContent);
+        $eventregistrationenddate = $this->deserializeDate($eventElement->getElementsByTagName('eventregistrationenddate')->item(0)->textContent);
+        $eventregistrationstartdate = $this->deserializeDate($eventElement->getElementsByTagName('eventregistrationstartdate')->item(0)->textContent);
+        $eventstartdate = $this->deserializeDate($eventElement->getElementsByTagName('eventstartdate')->item(0)->textContent);
+        $eventtype = $eventElement->getElementsByTagName('eventtype')->item(0)->textContent;
+        $friendlyurl = $eventElement->getElementsByTagName('friendlyurl')->item(0)->textContent;
+        $physicalurl = $eventElement->getElementsByTagName('physicalurl')->item(0)->textContent;
+        $dataxml = $eventElement->getElementsByTagName('dataxml')->item(0)->textContent;
+        $metadataxml = $eventElement->getElementsByTagName('metadataxml')->item(0)->textContent;
+
 
         $e = new Event();
         $e->setDocumentname($documentname);
         $e->setDocumentdescription($documentdescription);
         $e->setEventenddate($eventenddate);
         $e->setEventonline($eventonline);
-        $e->setEventregistrationenddate(new \DateTime());
-        $e->setEventregistrationstartdate(new \DateTime());
-        $e->setEventstartdate(new \DateTime());
-        $e->setEventtype('prueba');
-        $e->setFriendlyurl('prueba');
-        $e->setPhysicalurl('prueba');
-        $e->setDataxml('prueba');
-        $e->setMetadataxml('prueba');
+        $e->setEventregistrationenddate($eventregistrationenddate);
+        $e->setEventregistrationstartdate($eventregistrationstartdate);
+        $e->setEventstartdate($eventstartdate);
+        $e->setEventtype($eventtype);
+        $e->setFriendlyurl($friendlyurl);
+        $e->setPhysicalurl($physicalurl);
+        $e->setDataxml($dataxml);
+        $e->setMetadataxml($metadataxml);
 
         return $e;
     }
